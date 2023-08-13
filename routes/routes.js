9544a123d1b20a76704fa8c5146dfe2d5987fb3e
Anukul Saini <anukulsaini@Anukuls-MacBook-Air.local>
2023-08-13 18:31:16 +0530

    second

--- routes/routes.js
+++ routes/routes.js
@@ -15,24 +15,103 @@ router.post('/register',async(req,res)=>{
     let password = req.body.password
     let name = req.body.name
 
-    const user = new User({
-        name:name,
-        email:email,
-        password:password
-    })
+    const salt = await bcrypt.genSalt(10)
+    
+    const hashedPassword = await bcrypt.hash(password,salt)
 
-    const result = await user.save()
-    res.json({
-        user:result
-    })
+    const record = await User.findOne({email:email})
+
+    if(record){
+        return res.status(400).send({
+            message:"Email is already registered"
+        })
+    }else{
+
+        const user = new User({
+            name:name,
+            email:email,
+            password:hashedPassword
+        })
+
+        const result = await user.save()
+
+        //jwt token
+
+        const {_id} = await result.toJSON()
+
+        const token = jwt.sign({_id:_id},"secret")
+
+        res.cookie("jwt",token,{
+            httpOnly:true,
+            maxAge:24*60*1000//in  millisecond
+        })
+
+       res.send({
+        message:"Success"
+       })
+
+    }
 })
 
 router.post("/login",async (req,res)=>{
-    res.send("login user")
+    const user = await User.findOne({email:req.body.email})
+
+    if(!user){
+        return  res.status(404).send({
+            message:"User not found",
+        });
+    }
+
+    if(!(await bcrypt.compare(req.body.password,user.password))){
+        return  res.status(400).send({
+            message:"Password is Incorrect"
+        });
+    }
+
+    const token = jwt.sign({_id:user._id},"secret");
+
+    res.cookie("jwt",token,{
+        httpOnly:true,
+        maxAge:24*60*60*1000//one day
+    })
+
+    res.send({
+        message:"success"
+    })
+
 })
 
-router.get('/user',(req,res)=>{
-    res.send("user")
+router.get('/user',async (req,res)=>{
+    try{
+            const cookie = req.cookies['jwt']
+
+            const claims = jwt.verify(cookie,"secret")
+            //check jwt token present inside user browser or not wit jwt.verify method
+
+            if(!claims){
+                return res.status(401).send({
+                    message:"Unauthenticated"
+                })
+            }
+
+            const user = await User.findOne({_id:claims._id})
+
+            const {password,...data} = await user.toJSON()
+            res.send(data)
+    }
+    catch(err){
+        return res.status(401).send({
+            message:'unauthenticated'
+        })
+    }
+})
+//delete the cookie whrn loggingout
+router.post('/logOut',(req,res)=>{
+    res.cookie("jwt","",{maxAge:0})
+
+    res.send({
+        message:"success"
+    })
 })
 
 module.exports = router
